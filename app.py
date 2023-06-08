6de90c6302552e0150bc01ed66fc241c30f46496
Gaurav Pandey <45666264+JeeKayPee@users.noreply.github.com>
2023-06-08 11:04:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import random
+quotes = [
+    "Be yourself; everyone else is already taken.",
+    "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    "In three words I can sum up everything I've learned about life: it goes on."
+]
